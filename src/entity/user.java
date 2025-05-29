f084f6ed2b2b7edc3e8d47904b747292317b7eb8
Pankaj Urmaliya <pankajuramaliya19@gmail.com>
2025-05-29 09:45:58 +0530

    Initial commit : Add BookMyMovie project

---
+++ src/entity/user.java
@@ -0,0 +1,47 @@
+package entity;
+
+public class user {
+    private int userID;
+    private String name;
+    private String email;
+    private String phone;
+
+    public user(int userID, String name, String email, String phone) {
+        this.userID = userID;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
